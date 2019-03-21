0a956b7a918ae781b0cfd32e3f45f8024624d690
Ellaox <40073721+Ellaox@users.noreply.github.com>
2019-03-21 11:42:28 +0000

    Create week 7 loopThroughName.py
    week 7 exercise 5.1
    21/03/2019

---
+++ week 7 loopThroughName.py
@@ -0,0 +1,24 @@
+# loopThroughName.py
+# this program loops through a users first name
+# displaying each letter of their name on a separate line
+# D.H. January 5th 2005
+
+firstName = raw_input("Please enter your first name: ")
+print
+print "Each letter of your name follows "
+print
+
+for letter in firstName:
+     print letter
+
+>>> ================================ RESTART ================================
+>>> 
+Please enter your first name: Ella
+
+Each letter of your name follows 
+
+E
+l
+l
+a
+>>> 
